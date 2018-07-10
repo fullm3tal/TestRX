f6843d9b9ca6549d7b6ed83c992be5fc786b885e
Bheem.Negi <JayantArora@satincreditcare.com>
2018-07-10 18:41:55 +0530

    Maybe Done

--- app/src/test/java/com/example/bheemnegi/testrx/ActivityPresenterTest.java
+++ app/src/test/java/com/example/bheemnegi/testrx/ActivityPresenterTest.java
@@ -1,7 +1,5 @@
 package com.example.bheemnegi.testrx;
 
-import android.widget.ListView;
-
 import com.example.bheemnegi.testrx.mvp.model.DataRepository;
 import com.example.bheemnegi.testrx.mvp.presenter.ActivityPresenter;
 import com.example.bheemnegi.testrx.mvp.view.ActivityView;
@@ -13,19 +11,14 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnit;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.MockitoRule;
 
 import java.lang.ref.WeakReference;
-import java.util.ArrayList;
 import java.util.List;
 
 import io.reactivex.disposables.Disposable;
-import io.reactivex.observers.DisposableSingleObserver;
 
 import static org.mockito.ArgumentMatchers.matches;
-import static org.mockito.Mockito.atLeast;
-import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
@@ -67,7 +60,7 @@ public class ActivityPresenterTest {
         List userList= mock(List.class);
         userList.add(user);
 
-        Mockito.when(dataRepository.getUserData(listener)).thenReturn(disposable);
+        Mockito.when(dataRepository.getObservableUserData(listener)).thenReturn(disposable);
 
         verify(userList).add(user);
         verify(userList).clear();
